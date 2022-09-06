def601588ef6c2dd09d143b270b8c40130a370fe
Alex Robinson <robin3a5@mail.uc.edu>
2022-09-06 14:51:20 -0400

    Add javadoc, add default / endpoint

--- src/main/java/com/plantplaces/PlantPlacesController.java
+++ src/main/java/com/plantplaces/PlantPlacesController.java
@@ -3,13 +3,23 @@ package com.plantplaces;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+/**
+ * Handle the /start endpoint
+ * return
+ */
 @Controller
 public class PlantPlacesController {
 
+	
 	@RequestMapping("/start")
 	public String start() {
 		return "start";
 		
 	}
 	
+	@RequestMapping("/index")
+	public String index() {
+		return "start";
+		
+	}
 }
